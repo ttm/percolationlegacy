e9916f42d42f212d157c0b7566bdaf2d494969ca
Renato Fabbri <renato.fabbri@gmail.com>
2016-01-06 05:15:09 -0200

    terminada adequacao das medidas wordnet

--- percolation/text/wordnet.py
+++ percolation/text/wordnet.py
@@ -66,22 +66,35 @@ def medidasWordnet(wordnet_context,pos_tag=None):
         tagged_words_chosen=[i[1] for i in tagged_words if i[1].pos() in pos_tag]
     else:
         tagged_words_chosen=[i[1] for i in tagged_words]
-    hyperpaths=[i.hypernym_paths() for i in tagged_words_chosen]
+    hyperpaths_word=[i.hypernym_paths() for i in tagged_words_chosen]
+    hyperpaths=[i for j in hyperpaths_words for i in j]
     lemmas=[i.lemmas() for i in tagged_words_chosen] ###
-    tmember_holonyms=[i.member_holonyms() for i in tagged_words_chosen]
-    part_holonyms=[i.part_holonyms() for i in tagged_words_chosen]
+
+    tmember_holonyms  =[i.member_holonyms() for i in tagged_words_chosen]
+    part_holonyms     =[i.part_holonyms() for i in tagged_words_chosen]
     substance_holonyms=[i.substance_holonyms() for i in tagged_words_chosen]
+    holonyms=[i+j+l for i,j,l in zip(tmember_holonyms,part_holonyms,substance_holonyms)]
+
+
     tmember_meronyms=   [i.member_meronyms() for i in tagged_words_chosen] #
     part_meronyms=     [i.part_meronyms() for i in tagged_words_chosen]
     substance_meronyms=[i.substance_meronyms() for i in tagged_words_chosen]
-    entailments=[i.entailments() for i in tagged_words_chosen]
-    hypernyms=[i.hypernyms() for i in tagged_words_chosen]
-    instance_hypernyms=[i.instance_hypernyms() for i in tagged_words_chosen]
-    hyponyms=[i.hyponyms() for i in tagged_words_chosen] ###
-    instance_hyponyms=[i.instance_hyponyms() for i in tagged_words_chosen]
+    meronyms=[i+j+l for i,j,l in zip(tmember_meronyms,part_meroyms,substance_meronyms)]
+
     region_domains=[i.region_domains() for i in tagged_words_chosen] #
     topic_domains= [i.topic_domains()  for i in tagged_words_chosen]
     usage_domains= [i.usage_domains()  for i in tagged_words_chosen]
+    domains=[i+j+l for i,j,l in zip(region_domains,topic_domains,usage_domains)]
+
+    hypernyms=[i.hypernyms() for i in tagged_words_chosen]
+    instance_hypernyms=[i.instance_hypernyms() for i in tagged_words_chosen]
+    hypernyms_all=[i+j for i,j in zip(hypernyms,instance_hypernyms)]
+
+    hyponyms=[i.hyponyms() for i in tagged_words_chosen] ###
+    instance_hyponyms=[i.instance_hyponyms() for i in tagged_words_chosen]
+    hyponyms_all=[i+j for i,j in zip(hyponyms,instance_hyponyms)]
+
+    entailments=[i.entailments() for i in tagged_words_chosen]
     similar=[    i.similar_tos() for i in tagged_words_chosen]
     verb_groups=[i.verb_groups() for i in tagged_words_chosen]
 
@@ -91,57 +104,10 @@ def medidasWordnet(wordnet_context,pos_tag=None):
     tdict={"tmax_depth":max_depth,"tmin_depth":min_depth}
     measures["lengths"].update(tdict)
     measures["numeric"].update(P.text.aux.mediaDesvioNumbers(tdict))
-    top_hypernyms=[i[0][:4] for j in hyperpaths for i in j] # fazer histograma por camada
+    top_hypernyms=[i[0][:4] for i in hyperpaths] # fazer histograma por camada
     lexnames=[i.lexname().split(".")[-1] for i in tagged_words_chosen] # rever
     measures["strings"]["top_hypernyms"]=top_hypernyms
     measures["strings"]["lexnames"]=lexnames
 
-
-
-
-    nlemmas=[len(i.lemmas()) for i in tagged_words_chosen] ###
-    nhyperpaths=[len(i) for i in hyperpaths] # number of hyperpaths in each word
-    shyperpaths=[len(i) for j in hyperpaths for i in j] # number of elements in each hyperpath
-
-    top_hypernyms=[i[0][:4] for j in hyperpaths for i in j] # fazer histograma por camada
-    lexnames=[i.lexname().split(".")[-1] for i in tagged_words_chosen] # rever
-
-    member_holonyms=[len(i.member_holonyms()) for i in tagged_words_chosen]
-    part_holonyms=[len(i.part_holonyms()) for i in tagged_words_chosen]
-    substance_holonyms=[len(i.substance_holonyms()) for i in tagged_words_chosen]
-    nholonyms=[mhol[i]+phol[i]+shol[i] for i in range(len(tagged_words_chosen))] ###
-
-    member_meronyms=[len(i.member_meronyms()) for i in tagged_words_chosen] #
-    part_meronyms=[len(i.part_meronyms()) for i in tagged_words_chosen]
-    substance_meronyms=[len(i.substance_meronyms()) for i in tagged_words_chosen]
-    nmeronyms=[mmer[i]+pmer[i]+smer[i] for i in range(len(tagged_words_chosen))] ###
-
-    nentailments=[len(i.entailments()) for i in tagged_words_chosen]
-
-    nhypernyms=[len(i.hypernyms()) for i in tagged_words_chosen]
-    ninstance_hypernyms=[len(i.instance_hypernyms()) for i in tagged_words_chosen]
-    nhypernyms=[nhypernyms[i]+ninstance_hypernyms[i] for i in range(len(tagged_words_chosen))]
-
-    nhyponyms=[len(i.hyponyms()) for i in tagged_words_chosen] ###
-    ninstance_hyponyms=[len(i.instance_hyponyms()) for i in tagged_words_chosen]
-    nhyponyms=[nhyponyms[i]+ninstance_hyponyms[i] for i in range(len(tagged_words_chosen))]
-
-    max_depth=[i.max_depth() for i in tagged_words_chosen] ###
-    min_ddepth=[i.min_depth() for i in tagged_words_chosen] ###
-
-    nregion_domains=[len(i.region_domains()) for i in tagged_words_chosen] #
-    ntopic_domains= [len(i.topic_domains())  for i in tagged_words_chosen]
-    nusage_domains= [len(i.usage_domains())  for i in tagged_words_chosen]
-    ndomains=[nregion_domains[i]+ntopic_domains[i]+nusage_domains[i]
-            for i in range(len(tagged_words_chosen))] ###
-
-    nsimilar=[    len(i.similar_tos()) for i in tagged_words_chosen]
-    nverb_groups=[len(i.verb_groups()) for i in tagged_words_chosen]
-
-    del wordnet_context,pos_tag,tagged_words
-    locals_=locals()
-    mvars_=[i for i in locals_.keys() if i not in ("tagged_words_chosen","hyperpaths","top_hypernyms","lexnames")]
-    medidas=mediaDesvio(mvars,locals_)
-    medidas.update(locals_)
-    return medidas
+    return measures
 
